b4bf25948c1f3667bfc612e9492ad7fff0d0e11b
KAKOnyx <Kolesen@students.mags.school.nz>
2026-08-26 14:02:17 +1200

    commit

---
+++ script.js
@@ -0,0 +1,17 @@
+// get the button and dropdown content elements
+const dropdownbutton = document.getElementById('dropdownbutton');
+const dropdowncontent = document.getElementById('dropdowncontent');
+
+// listen for clicks on the dropdown button
+dropdownbutton.addEventListener('click',() => {
+// toggle the visibility of the dropdown content
+    dropdowncontent.style.display = dropdowncontent.style.display === 'block' ? 'none' : 'block';
+});
+
+// close the dropdown if the user clicks outside of it
+window.addEventListener('click', (event) => {
+    if (!event.target.matches('.dropdown button')) {
+        //close dropdown if the click is outside
+        dropdowncontent.style.display = 'none';
+    }
+});
